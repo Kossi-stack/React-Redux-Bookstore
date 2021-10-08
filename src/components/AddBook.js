622acf5e4c6af5c2bbd7450cf6fdca11be47004f
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-07 20:52:24 -0700

    Delete node_modules and re-install linters

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from './redux/Books/Books';
+import { addBook } from '../redux/Books/Books';
 
 const AddBook = () => {
   const dispatch = useDispatch();
