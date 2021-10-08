98a736b7dcb9b8c60a9616c805560c1d9939739f
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-08 06:28:28 -0700

    Add removeBook function

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/books';
+import { addBook } from '../redux/books/books.js';
 
 const AddBook = () => {
   const dispatch = useDispatch();
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { removeBook } from '../redux/books/books.s';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
