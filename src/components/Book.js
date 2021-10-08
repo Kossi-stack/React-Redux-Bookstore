b32babc9bedf0d54e839c841aa92b780503a6ef7
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-07 21:26:27 -0700

    linter fix

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/Books/Books';
+import { removeBook } from '../redux/books/books';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
