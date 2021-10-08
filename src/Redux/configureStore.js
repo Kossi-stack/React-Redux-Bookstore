1dfa1122baaab25c319e81488e088836ebff28c6
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-08 06:09:34 -0700

    Update readme file

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import booksReducer from './Books/Books';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
