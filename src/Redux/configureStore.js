1a404413d3cec548c0c0cd03d2581ad8f8b22677
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-10-09 14:19:27 -0700

    Add more styling

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import booksReducer from '.Books/Books';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
   books: booksReducer,
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import store from './Redux/configureStore';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
